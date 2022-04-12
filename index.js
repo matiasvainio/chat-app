a4aa4c5ecaeaabb7b1ab4c7059fa5f4bfa143937
Matias Vainio <matias.s.vainio@gmail.com>
2022-04-12 19:37:48 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+http = require('http')
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('Hello world!')
+})
+
+server.listen(3001, () => {
+  console.log('server running')
+})
