75a3c537e1720af428a3cf53360209226f2e55c7
Matias Vainio <matias.s.vainio@gmail.com>
2022-04-13 13:47:34 +0300

    moved files to backend folder

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 express = require('express')
 const app = express()
 
+app.use('/chat', (req, res, next) => {
+  res.send('<h1>Chat</h1>')
+})
+
 app.use('/', (req, res, next) => {
   res.send('hello world')
 })
