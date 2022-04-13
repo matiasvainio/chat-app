8ee180bdbd0ac8089804f902d284df1b95b13718
Matias Vainio <matias.s.vainio@gmail.com>
2022-04-13 22:47:11 +0300

    basic functionality for chat added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { io } from 'socket.io-client'
+import { ObjectId } from 'bson'
+import { useEffect, useState } from 'react'
+
+const socket = io.connect('localhost:3002')
 
 function App() {
+  const [messages, setMessages] = useState([])
+  const [input, setInput] = useState('')
+
+  useEffect(() => {
+    if (messages.length === 0) {
+      socket.on('chat messages', (messages) => {
+        console.log('chat messages useeffect')
+        setMessages(messages)
+      })
+    }
+  })
+
+  useEffect(() => {
+    socket.on('chat message', (message) => {
+      setMessages([...messages, message])
+    })
+  })
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const message = {
+      _id: new ObjectId().toString(),
+      content: input,
+    }
+
+    socket.emit('chat message', message)
+    setInput('')
+  }
+
+  const handleInput = (event) => {
+    event.preventDefault()
+    setInput(event.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world</h1>
+      {messages.map((x) => (
+        <li key={x._id}>{x.content}</li>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Type..."
+          onChange={handleInput}
+          value={input}
+        ></input>
+        <button>Submit</button>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
