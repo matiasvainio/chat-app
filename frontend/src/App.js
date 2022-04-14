c45cb8d3b573baef3843059433a53fbca54d6fd2
Matias Vainio <matias.s.vainio@gmail.com>
2022-04-14 16:04:51 +0300

    landing and settings components, refactor socket-client

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,13 +12,16 @@ function App() {
       <nav>
         <Link to="login">login</Link>
         <Link to="rooms">rooms</Link>
+        <Link to="settings">settings</Link>
       </nav>
       <Routes>
-        <Route path="/" element={<Home />}>
+        <Route path="/" element={<Landing />}>
+          <Route path="home" element={<Home />} />
           <Route path="login" element={<Login />} />
           <Route path="rooms" element={<Rooms />}>
             <Route path=":id" element={<Room />} />
           </Route>
+          <Route path="settings" element={<Settings />} />
         </Route>
       </Routes>
     </div>
@@ -30,12 +33,15 @@ const Chat = ({ roomid }) => {
   const [input, setInput] = useState('')
 
   useEffect(() => {
-    if (messages.length === 0) {
-      socket.on('chat messages', (messages) => {
-        console.log('chat messages useeffect')
-        setMessages(messages)
-      })
-    }
+    socket.off('chat messages')
+    socket.off('chat message')
+  })
+
+  useEffect(() => {
+    socket.on('chat messages', (messages) => {
+      console.log('chat messages useeffect', socket._callbacks)
+      setMessages(messages)
+    })
   })
 
   useEffect(() => {
@@ -52,6 +58,7 @@ const Chat = ({ roomid }) => {
       content: input,
       date: new Date(),
       roomid: roomid,
+      user: window.localStorage.getItem('username'),
     }
 
     socket.emit('chat message', message)
@@ -76,26 +83,57 @@ const Chat = ({ roomid }) => {
           onChange={handleInput}
           value={input}
         ></input>
-        <button>Submit</button>
+        <button>submit</button>
       </form>
     </div>
   )
 }
 
 const Message = ({ props }) => {
-  const { content, _id, date } = props
-  return <li>{content}</li>
+  const { content, _id, date, user } = props
+  return (
+    <li>
+      <p>{content}</p>
+      <p>{user}</p>
+    </li>
+  )
 }
 
 const Login = () => {
   return (
     <div>
-      <h1>Login</h1>
+      <h1>login</h1>
       <form>
         <input placeholder="Username"></input>
         <input placeholder="Password"></input>
-        <button>Submit</button>
+        <button>submit</button>
       </form>
+    </div>
+  )
+}
+
+const Landing = () => {
+  const [username, setUsername] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    window.localStorage.setItem('username', username)
+    setUsername('')
+  }
+
+  const handleInput = (event) => {
+    event.preventDefault()
+    setUsername(event.target.value)
+  }
+
+  return (
+    <div>
+      <h1>username:</h1>
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleInput} value={username} />
+        <button>submit</button>
+      </form>
+      <Outlet />
     </div>
   )
 }
@@ -103,20 +141,20 @@ const Login = () => {
 const Home = () => {
   return (
     <div>
-      <Outlet />
+      <h1>Home</h1>
     </div>
   )
 }
 
 const Rooms = () => {
   const [rooms, setRooms] = useState([
-    { id: 1, name: 'General' },
-    { id: 2, name: 'Politics' },
+    { id: 1, name: 'general' },
+    { id: 2, name: 'politics' },
   ])
 
   return (
     <div>
-      <h1>Rooms</h1>
+      <h1>rooms</h1>
       <ul style={{ margin: 0, padding: 0 }}>
         {rooms.map((room) => (
           <li style={{ listStyle: 'none' }} key={room.id}>
@@ -133,10 +171,20 @@ const Room = () => {
   const roomname = useParams()
   console.log(`join room ${roomname.id}`)
   socket.emit('join room', roomname.id)
+  // socket.off('chat messages')
+  // socket.off('chat message')
   return (
     <div>
       <h1>{roomname.id}</h1>
       <Chat roomid={roomname.id} />
+    </div>
+  )
+}
+
+const Settings = () => {
+  return (
+    <div>
+      <h1>Settings</h1>
     </div>
   )
 }
