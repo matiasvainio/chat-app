6e9752ca882b3e98ffba61c9b2419f9340e4f294
Matias Vainio <matias.s.vainio@gmail.com>
2022-04-20 19:58:42 +0300

    message routing extracted to its own file

---
+++ backend/routes/messageRouter.js
@@ -0,0 +1,35 @@
+const Message = require('../models/message')
+const express = require('express')
+const messageRouter = express.Router()
+
+messageRouter.get('/', async (_req, res) => {
+  const messages = await Message.find({})
+  res.status(200).json(messages)
+})
+
+messageRouter.post('/', async (req, res) => {
+  const body = req.body
+  if (body.content === undefined) {
+    return res.status(401).json({ error: 'content missing' })
+  }
+
+  const message = new Message({
+    content: body.content,
+    user: body.user,
+    date: new Date(),
+    roomid: String,
+  })
+
+  await message.save()
+
+  const messages = await Message.find({})
+
+  res.status(200).json(messages)
+})
+
+messageRouter.post('/clear', async (_req, res) => {
+  await Message.deleteMany({})
+  res.status(200).json({ content: 'messages removed' })
+})
+
+module.exports = messageRouter
