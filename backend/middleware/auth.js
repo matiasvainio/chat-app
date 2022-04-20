bad601dbe5c6129be178b799ccb454ef53df3556
Matias Vainio <matias.s.vainio@gmail.com>
2022-04-20 19:57:58 +0300

    verifytoken function extracted to its own file

---
+++ backend/middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+
+const verifytoken = (req, res, next) => {
+  const auth = req.headers.authorization
+  if (auth && auth.toLowerCase().startsWith('bearer ')) {
+    req.token = auth.substring(7)
+  }
+
+  if (!req.token) {
+    return res.status(403).json({ error: 'token required for authentication' })
+  }
+
+  try {
+    const decoded = jwt.verify(req.token, process.env.JWT_SECRET_KEY)
+    req.user = decoded
+  } catch (err) {
+    console.log(err)
+  }
+
+  return next()
+}
+
+module.exports = verifytoken
