42d75679447970d03822766afa269f5996b6a77a
Matias Vainio <matias.s.vainio@gmail.com>
2022-04-20 19:58:29 +0300

    user schema extracted to its own file

---
+++ backend/models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, default: null },
+  email: { type: String, unique: true },
+  password: String,
+  token: String,
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
