e865fadd7ae67f505af9be97279c335b36d55bb9
Matias Vainio <matias.s.vainio@gmail.com>
2022-04-25 15:28:45 +0300

    room schema has room's creator user id stored

--- backend/models/room.js
+++ backend/models/room.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose')
 
 const roomSchema = mongoose.Schema({
   name: { type: String, unique: true, null: false },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  private: { type: Boolean, null: false },
 })
 
 const Room = mongoose.model('Room', roomSchema)
