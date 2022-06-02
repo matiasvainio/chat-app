f96dd9736887254a883bcd221e22c1a9fcf457b5
Matias Vainio <matias.s.vainio@gmail.com>
2022-06-02 12:58:17 +0300

    rename && move context to it's own folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
-import React, { useState, createContext } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
-import SocketContext, { socket } from './components/SocketContext'
+import SocketContext, { socket } from './contexts/socketContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
